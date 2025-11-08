5b8599203837b4c7f520f05ea2d8fec65b640c87
AyoubEsprit <Ayoub.Belhameid@esprit.tn>
2025-11-08 21:25:40 +0100

    Ajout de l'intégration de la partie SpringDoc (Swagger)

--- src/main/java/tn/esprit/twin/projectspring/Controllers/ClientRestController.java
+++ src/main/java/tn/esprit/twin/projectspring/Controllers/ClientRestController.java
@@ -1,5 +1,7 @@
 package tn.esprit.twin.projectspring.Controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.twin.projectspring.entities.Client;
@@ -8,11 +10,13 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Gestion des clients")
 @RequestMapping("/api/client")
 public class ClientRestController {
 
   IClientServices clientService;
 
+  @Operation(description = "Récupérer tous les clients de la base de données")
   @GetMapping("/retrieve-all-clients")
   public List<Client> getClients() {
     return clientService.retrieveALLClients();
@@ -23,16 +27,19 @@ public class ClientRestController {
     return clientService.retrieveClient(id);
   }
 
+  @Operation(description = "Ajouter un nouveau client")
   @PostMapping("/add-client")
   public Client addClient(@RequestBody Client c) {
     return clientService.addClient(c);
   }
 
+  @Operation(description = "Mettre à jour un client existant")
   @PutMapping("/update-client")
   public Client updateClient(@RequestBody Client c) {
     return clientService.updateClient(c);
   }
 
+  @Operation(description = "Supprimer un client par ID")
   @DeleteMapping("/remove-client/{client-id}")
   public void removeClient(@PathVariable("client-id") Long id) {
     clientService.removeClient(id);
--- src/main/java/tn/esprit/twin/projectspring/Controllers/CommandeRestController.java
+++ src/main/java/tn/esprit/twin/projectspring/Controllers/CommandeRestController.java
@@ -1,6 +1,9 @@
 package tn.esprit.twin.projectspring.Controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.twin.projectspring.entities.Commande;
 import tn.esprit.twin.projectspring.services.ICommandeServices;
@@ -9,11 +12,13 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Gestion des commandes")
 @RequestMapping("/api/commande")
 public class CommandeRestController {
 
     ICommandeServices commandeService;
 
+    @Operation(description = "Récupérer tous les commandes de la base de données")
     @GetMapping("/retrieve-all-commandes")
     public List<Commande> getCommandes() {
         return commandeService.retrieveAllCommandes();
@@ -24,16 +29,19 @@ public class CommandeRestController {
         return commandeService.retrieveCommande(id);
     }
 
+    @Operation(description = "Ajouter une nouvelle commande")
     @PostMapping("/add-commande")
     public Commande addCommande(@RequestBody Commande c) {
         return commandeService.addCommande(c);
     }
 
+    @Operation(description = "Mettre à jour une commande existante")
     @PutMapping("/update-commande")
     public Commande updateCommande(@RequestBody Commande c) {
         return commandeService.updateCommande(c);
     }
 
+    @Operation(description = "Supprimer une commande par ID")
     @DeleteMapping("/remove-commande/{commande-id}")
     public void removeCommande(@PathVariable("commande-id") Long id) {
         commandeService.removeCommande(id);
